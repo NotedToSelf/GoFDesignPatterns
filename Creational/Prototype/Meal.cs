262fe3c04aa9f838d110b66f9ae28401cc025f41
Erik Bogeberg <Erik.Bogeberg@blackline.com>
2019-05-13 18:01:35 -0700

    Prototype: added Meals base class and subclasses

---
+++ Creational/Prototype/Meal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+
+    public enum Drink
+    {
+        Coke,
+        Pepsi,
+        Sprite,
+        IcedTea,
+        Coffee,
+        MtnDew,
+        MilkShake
+    }
+
+    public enum Side
+    {
+        Fries,
+        Salad,
+        OnionRings,
+        TaterTots,
+    }
+
+    /// <summary>
+    /// Represents a fast food meal Prototype
+    /// </summary>
+    abstract class Meal
+    {
+        private string name;
+        private Drink[] drinks;
+        private Side[] sides;
+        private float total; //Adjusted by certain sides or drinks
+
+        public abstract void Clone();
+    }
+
+    class Burger : Meal
+    {
+        Burger()
+        {
+
+        }
+
+        public override void Clone()
+        {
+
+        }
+
+    }
+
+    class Salad : Meal
+    {
+        Salad ()
+        {
+
+        }
+
+        public override void Clone()
+        {
+
+        }
+    }
+
+}
--- Creational/Prototype/Program.cs
+++ Creational/Prototype/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Prototype
 {
+
+    /// <summary>
+    /// Implementation of Prototype Design Pattern.
+    /// Fast Food ordering system that leverages Prototype to more easily create or duplicate meals from their generic templates.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Meal> myOrder = new List<Meal>();
         }
     }
 }
