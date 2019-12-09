7dd72ba9cdb3edc6f53f38f46089f197cbd6c053
NotedToSelf <notedtoself@gmail.com>
2019-12-09 02:45:36 -0800

    Working on Factory method, variable names to be changed

--- Creational/Factory Method/Program.cs
+++ Creational/Factory Method/Program.cs
@@ -3,25 +3,21 @@
 namespace FactoryMethod
 {
     class MainApp
-
     {
 
         static void Main()
         {
             // An array of creators
-
             Creator[] creators = new Creator[2];
 
             creators[0] = new ConcreteCreatorA();
             creators[1] = new ConcreteCreatorB();
 
             // Iterate over creators and create products
-
             foreach (Creator creator in creators)
             {
                 Product product = creator.FactoryMethod();
-                Console.WriteLine("Created {0}",
-                  product.GetType().Name);
+                Console.WriteLine("Created {0}", product.GetType().Name);
             }
 
             Console.ReadKey();
@@ -29,28 +25,24 @@ namespace FactoryMethod
     }
 
     abstract class Product
-
     {
     }
 
     class ConcreteProductA : Product
-
     {
     }
 
     class ConcreteProductB : Product
-
     {
     }
 
     abstract class Creator
-
     {
         public abstract Product FactoryMethod();
     }
 
+    // Constructs an object of type
     class ConcreteCreatorA : Creator
-
     {
         public override Product FactoryMethod()
         {
@@ -58,8 +50,8 @@ namespace FactoryMethod
         }
     }
 
+    // Constructs an object of type
     class ConcreteCreatorB : Creator
-
     {
         public override Product FactoryMethod()
         {
