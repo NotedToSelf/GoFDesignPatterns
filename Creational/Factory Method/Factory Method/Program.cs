edfd16460d8748c08e06d938bff40f5bdafa29cb
NotedToSelf <notedtoself@gmail.com>
2019-12-08 22:33:36 -0800

    Basic Factory Method

---
+++ Creational/Factory Method/Factory Method/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class MainApp
+
+    {
+
+        static void Main()
+        {
+            // An array of creators
+
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            // Iterate over creators and create products
+
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}",
+                  product.GetType().Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Product
+
+    {
+    }
+
+    class ConcreteProductA : Product
+
+    {
+    }
+
+    class ConcreteProductB : Product
+
+    {
+    }
+
+    abstract class Creator
+
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator
+
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    class ConcreteCreatorB : Creator
+
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
