ad96326358f83aac7b19e1436cf59dd137adbd7b
NotedToSelf <notedtoself@gmail.com>
2019-12-08 22:40:38 -0800

    Bridge skeleton

---
+++ Structural/Bridge/Bridge/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Bridge
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            Abstraction ab = new RefinedAbstraction();
+
+            // Set implementation and call
+
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+
+            // Change implemention and call
+
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    class Abstraction
+
+    {
+        protected Implementor implementor;
+
+        // Property
+
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    abstract class Implementor
+
+    {
+        public abstract void Operation();
+    }
+
+    class RefinedAbstraction : Abstraction
+
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    class ConcreteImplementorA : Implementor
+
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorA Operation");
+        }
+    }
+
+    class ConcreteImplementorB : Implementor
+
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorB Operation");
+        }
+    }
+}
