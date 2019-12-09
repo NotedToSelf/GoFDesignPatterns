ef833de0dd35781ed1ce5ebccc69a9628188447d
NotedToSelf <notedtoself@gmail.com>
2019-12-09 01:19:59 -0800

    Template skeleton

---
+++ Behavioral/Template/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Template
+{
+
+    class MainApp
+    {
+
+        static void Main()
+        {
+            AbstractClass aA = new ConcreteClassA();
+            aA.TemplateMethod();
+
+            AbstractClass aB = new ConcreteClassB();
+            aB.TemplateMethod();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class AbstractClass
+
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+
+        // The "Template method"
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine("");
+        }
+    }
+
+    class ConcreteClassA : AbstractClass
+
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+        }
+    }
+
+    class ConcreteClassB : AbstractClass
+
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+        }
+    }
+}
